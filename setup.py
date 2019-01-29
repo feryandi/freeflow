6ff38dcdfd9373dbe91d2d69533b2863295b6e15
feryandi <fnurdiantoro@traveloka.com>
2019-01-29 14:28:44 +0700

    Prevent pandas to be upgraded

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='freeflow',
-      version='0.1.5',
+      version='0.1.6',
       description='Airflow development and deployment, simplified.',
       url='http://github.com/feryandi/freeflow',
       author='Feryandi Nurdiantoro',
@@ -17,6 +17,7 @@ setup(name='freeflow',
         'google-cloud-bigquery==1.8.1',
         'pendulum==2.0.4',
         'pandas-gbq==0.6.1',
+        'pandas==0.23.4',
         'pytest==4.1.1',
         'oauth2client==4.1.3',
         'google-cloud-storage==1.13.2'
